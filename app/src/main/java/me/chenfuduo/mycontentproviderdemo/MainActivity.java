7ffba9b45104a3c4281cfa928919e9d71be97313
chenlouis <619189810@qq.com>
2015-06-16 08:14:21 +0800

    Initial commit

---
+++ app/src/main/java/me/chenfuduo/mycontentproviderdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package me.chenfuduo.mycontentproviderdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
